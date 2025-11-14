00e0f04159f7309e6883e86f4d55d7cc9d2563d9
realkoreantalk <koreanteacherhannah@gmail.com>
2025-11-15 02:56:12 +0900

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyACN_fkK12XD7BJ5mMwfm3n2IqPq0wMGVM",
+  authDomain: "real-korean-talk.firebaseapp.com",
+  projectId: "real-korean-talk",
+  storageBucket: "real-korean-talk.firebasestorage.app",
+  messagingSenderId: "236722130766",
+  appId: "1:236722130766:web:cdf573273bc884ed17d81f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
